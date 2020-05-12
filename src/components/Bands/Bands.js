4132f8679510cba6a3739ec5b69c50122564a34d
ablades <audaris@gmail.com>
2020-05-12 11:05:42 -0700

    add countries of origin

--- src/components/Bands/Bands.js
+++ src/components/Bands/Bands.js
@@ -4,15 +4,30 @@ import data from '../../metal.json'
 import './Bands.css'
 
 function Bands() {
-    const bands = data.map(({ID}) =>{
+    let countries = []
+    const bands = data.map(({ID, origin}) =>{
+        countries[origin] = (countries[origin] || 0) + 1
+
         return (
         <Band ID={ID} />)
     })
 
 
+    const countriesList = []
+
+    Object.entries(countries).map(([key,value]) => {
+        countriesList.push(<li>{key}: {value}</li>)
+    })
+
     return(
         <div>
+            <h3>Countries of origin</h3>
+            <ul>
+            {countriesList}
+            </ul>
+
             <h1># of Bands: {bands.length}</h1>
+            
             <div className="Bands">
             {bands}
             </div>
