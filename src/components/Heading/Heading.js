353c5e0fff7260af50eba037f654cad7785760d4
ablades <audaris@gmail.com>
2020-05-12 09:49:53 -0700

    add heading component

---
+++ src/components/Heading/Heading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Heading(){
+    return(
+        <div className="heading">
+            <title>FEW 1.2 Final Assessment</title>
+            <h3>Audi Blades</h3>
+        </div>
+    )
+}
+
+export default Heading
